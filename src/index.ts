ee662ffbb59f15397348cffd7989bc88c3b61518
Nuntapong Siripanyawong <nuntapong@odds.team>
2026-01-03 18:41:12 +0700

    Refactor API route structure and update server log message format

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .group("/api/v1", (app) => app.get("/hello", () => "Hello from API v1"))
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
